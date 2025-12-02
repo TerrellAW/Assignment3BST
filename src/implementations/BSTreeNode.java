d7997f3cc27b20e36cef0a0425a00ef1184ff097
TerrellAW <terraw1999@proton.me>
2025-12-01 19:04:05 -0700

    Add: Documentation

--- src/implementations/BSTreeNode.java
+++ src/implementations/BSTreeNode.java
@@ -1,10 +1,11 @@
 package implementations;
 
 /**
- * 
+ * Node that stores a value in a data structure.
  * 
  * @param <E> The type of elements this list holds.
  * @author TerrellAW
+ * @version 1.0
  */
 public class BSTreeNode<E>
 {
@@ -25,12 +26,21 @@ public class BSTreeNode<E>
 	 * Constructs the node object.
 	 *
 	 * @return Instance of the <code>BSTreeNode</code> class.
+	 * @author TerrellAW
+	 * @version 1.0
 	 */
 	BSTreeNode(E value) {
 		this.height = 1;
 		this.value = value;
 	}
 
+	/**
+	 * Returns the element stored in the node.
+	 *
+	 * @return the element stored in the node.
+	 * @author Estefano Campana
+	 * @version 1.0
+	 */
 	public E getElement() {
 		return this.value;
 	}
